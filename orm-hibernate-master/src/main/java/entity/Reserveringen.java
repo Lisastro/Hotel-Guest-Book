39293edf3d5d8c477986727b193ee854949ea2c3
Lisastro <ansjelisa1@gmail.com>
2023-02-12 11:40:36 -0300

    12

--- orm-hibernate-master/src/main/java/entity/Reserveringen.java
+++ orm-hibernate-master/src/main/java/entity/Reserveringen.java
@@ -11,20 +11,30 @@ public class Reserveringen {
     @GeneratedValue
     private Long id;
     private String reservering_nummer;
+    private String check_in;
+    private String check_out;
+    private String aantal_kamers;
+    private String aantal_personen;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public String getReservering_nummer() {
-        return reservering_nummer;
-    }
+    public String getReservering_nummer() { return reservering_nummer; }
+    public void setReservering_nummer(String reservering_nummer) { this.reservering_nummer = reservering_nummer; }
 
-    public void setReservering_nummer(String reservering_nummer) {
-        this.reservering_nummer = reservering_nummer;
-    }
+    public String getCheck_in() { return check_in; }
+    public void setCheck_in(String check_in) { this.check_in = check_in; }
+
+    public String getCheck_out() { return check_out; }
+    public void setCheck_out(String check_out) { this.check_out = check_out; }
+
+    public String getAantal_kamers() { return aantal_kamers; }
+    public void setAantal_kamers(String aantal_kamers) { this.aantal_kamers = aantal_kamers; }
+
+    public String getAantal_personen() { return aantal_personen; }
+    public void setAantal_personen(String aantal_personen) { this.aantal_personen = aantal_personen; }
+
+
+
 }
