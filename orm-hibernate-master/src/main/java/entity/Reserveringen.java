86ae9fb059238c1365bbc73ef7298e68b05aaf16
Lisastro <ansjelisa1@gmail.com>
2023-02-12 11:28:25 -0300

    12

--- orm-hibernate-master/src/main/java/entity/Reserveringen.java
+++ orm-hibernate-master/src/main/java/entity/Reserveringen.java
@@ -1,11 +1,30 @@
 package entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+
+@Entity
 public class Reserveringen {
     @Id
     @GeneratedValue
     private Long id;
-    private String naam;
+    private String reservering_nummer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReservering_nummer() {
+        return reservering_nummer;
+    }
+
+    public void setReservering_nummer(String reservering_nummer) {
+        this.reservering_nummer = reservering_nummer;
+    }
 }
