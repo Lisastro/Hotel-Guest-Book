36b18679004c26f30e4fee8e1d75e060815f90c7
Lisastro <ansjelisa1@gmail.com>
2023-02-25 15:49:40 -0300

    12

--- orm-hibernate-master/src/main/java/dp_ChainofResponsibility/SearchName.java
+++ orm-hibernate-master/src/main/java/dp_ChainofResponsibility/SearchName.java
@@ -3,7 +3,7 @@ package dp_ChainofResponsibility;
 public class SearchName extends SearchDemo{
 
         public void handleRequest(String searchQuery) {
-            if (searchQuery.contains("Gast")) {
+            if (searchQuery.contains("Naam")) {
                 System.out.println("Zoeken op naam: " + searchQuery);
             } else {
                 System.out.println("Geen zoekopdracht gevonden voor: " + searchQuery);
